aef06247f8385bd9029faab56a88dc7ae0006695
GabrielDeml <gabrielddeml@gmail.com>
2020-05-23 16:54:01 -0400

    seems to work on one folder

--- theConverter.py
+++ theConverter.py
@@ -17,13 +17,13 @@ def file_locations(folder):
 def print_json_data(file):
     with open(file) as f:
         data = json.load(f)
-        if data['objects'] is not None:
-            print(range(len(data['objects'])))
-            for i in range(len(data['objects'])):
-                print('point {} :'.format(i))
-                print(data['objects'][i]['points']['exterior'])
-        else:
-            print('There was nothing in objects in: ' + file)
+        # if data['objects'] is not None:
+        #     print(range(len(data['objects'])))
+        #     for i in range(len(data['objects'])):
+        #         print('point {} :'.format(i))
+        #         print(data['objects'][i]['points']['exterior'])
+        # else:
+        #     print('There was nothing in objects in: ' + file)
 
 
 def write_xml(cords, width, height, depth, filename, folder):
@@ -40,7 +40,6 @@ def write_xml(cords, width, height, depth, filename, folder):
     ET.SubElement(size, "height").text = str(height)
     ET.SubElement(size, "depth").text = str(depth)
 
-
     # object block
     for point in cords:
         object = ET.SubElement(annotation, "object")
@@ -56,8 +55,37 @@ def write_xml(cords, width, height, depth, filename, folder):
     tree.write(filename + ".xml")
 
 
+def convert_to_xml(folder_in, file_in, file_out):
+    with open(folder_in + file_in) as f:
+        data = json.load(f)
+        # find the cords
+        if data['objects'] is not None:
+            cords = []
+            print(range(len(data['objects'])))
+            for i in range(len(data['objects'])):
+                point = []
+                point.append(data['objects'][i]["classTitle"])
+                j = 0
+                while j < 2:
+                    k = 0
+                    while k < 2:
+                        point.append(data['objects'][i]['points']['exterior'][j][k])
+                        k = k + 1
+                    j = j + 1
+                cords.append(point)
+            print(cords)
+        else:
+            print('There was nothing in objects in: ' + file_in)
+        width = data['size']['width']
+        height = data['size']['height']
+        print(str(width) + " " + str(height))
+        write_xml(cords, width, height, 3, file_in, folder_in)
+        # write_xml(cords,)
+
+
 if __name__ == "__main__":
-    write_xml([["name", 1, 2, 3, 4], ["name2", 5, 6, 7, 8]], 1080, 1920, 3, "filename", "folder")
-        # list_of_files = file_locations('Filming Day 2 Video/ann/')
-        # print(list_of_files)
-        # print_json_data('Filming Day 2 Video/ann/frame_00000.png.json')
+    # write_xml([["name", 1, 2, 3, 4], ["name2", 5, 6, 7, 8]], 1080, 1920, 3, "filename", "folder")
+    # list_of_files = file_locations('Filming Day 2 Video/ann/')
+    # print(list_of_files)
+    # print_json_data('Filming Day 2 Video/ann/frame_00000.png.json')
+    convert_to_xml('Filming Day 2 Video/ann/', 'frame_00000.png.json', "null")
