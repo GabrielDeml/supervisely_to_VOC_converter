c11900bd0dc813d5d327dec90b47b2d8299e4b67
GabrielDeml <gabrielddeml@gmail.com>
2020-05-23 15:33:38 -0400

    Writing xml seems to work

--- theConverter.py
+++ theConverter.py
@@ -26,22 +26,38 @@ def print_json_data(file):
             print('There was nothing in objects in: ' + file)
 
 
-def write_test_xml(xmin, ymin, xmax, ymax, filename):
+def write_xml(cords, width, height, depth, filename, folder):
     annotation = ET.Element("annotation")
-    object = ET.SubElement(annotation, "object")
-    ET.SubElement(object, "name").text = filename
-    bndbox = ET.SubElement(object, "bndbox")
-    ET.SubElement(bndbox, "xmin").text = str(xmin)
-    ET.SubElement(bndbox, "ymin").text = str(ymin)
-    ET.SubElement(bndbox, "xmax").text = str(xmax)
-    ET.SubElement(bndbox, "ymax").text = str(ymax)
 
+    # folder
+    ET.SubElement(annotation, "folder").text = str(folder)
+    # filename
+    ET.SubElement(annotation, "filename").text = str(filename)
+
+    # size block
+    size = ET.SubElement(annotation, "size")
+    ET.SubElement(size, "width").text = str(width)
+    ET.SubElement(size, "height").text = str(height)
+    ET.SubElement(size, "depth").text = str(depth)
+
+
+    # object block
+    for point in cords:
+        object = ET.SubElement(annotation, "object")
+        ET.SubElement(object, "name").text = point[0]
+        bndbox = ET.SubElement(object, "bndbox")
+        ET.SubElement(bndbox, "xmin").text = str(point[1])
+        ET.SubElement(bndbox, "ymin").text = str(point[2])
+        ET.SubElement(bndbox, "xmax").text = str(point[3])
+        ET.SubElement(bndbox, "ymax").text = str(point[4])
+
+    # Write to a file
     tree = ET.ElementTree(annotation)
-    tree.write("filename.xml")
+    tree.write(filename + ".xml")
 
 
 if __name__ == "__main__":
-    write_test_xml(1, 2, 3, 4, "filename")
-    # list_of_files = file_locations('Filming Day 2 Video/ann/')
-    # print(list_of_files)
-    # print_json_data('Filming Day 2 Video/ann/frame_00000.png.json')
+    write_xml([["name", 1, 2, 3, 4], ["name2", 5, 6, 7, 8]], 1080, 1920, 3, "filename", "folder")
+        # list_of_files = file_locations('Filming Day 2 Video/ann/')
+        # print(list_of_files)
+        # print_json_data('Filming Day 2 Video/ann/frame_00000.png.json')
